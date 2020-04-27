c54e54e911d52e52c398c29f304add67f106814c
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2020-04-27 20:27:22 +0300

    Ex2 init

---
+++ Ex/ex2/ex2-solution/cmake-build-debug/CMakeFiles/ex2_solution.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex2_solution.dir/main.c.o"
+  "ex2_solution.exe"
+  "ex2_solution.pdb"
+  "libex2_solution.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ex2_solution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
